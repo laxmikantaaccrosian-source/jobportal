456db0b93f94af12e2a07c65463e9eeac08698cc
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-09-23 15:11:28 +0530

    first commit

---
+++ jp_app/logs/log-2025-08-13.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2025-08-13 01:10:32 --> Severity: 8192  --> Optional parameter $level declared before required parameter $msg is implicitly treated as a required parameter C:\xampp\htdocs\jobportal\jp_sys\libraries\Log.php 72
+ERROR - 2025-08-13 01:30:21 --> Severity: 8192  --> Optional parameter $level declared before required parameter $msg is implicitly treated as a required parameter C:\xampp\htdocs\jobportal\jp_sys\libraries\Log.php 72
+ERROR - 2025-08-13 04:28:17 --> Severity: 8192  --> Optional parameter $level declared before required parameter $msg is implicitly treated as a required parameter C:\xampp\htdocs\jobportal\jp_sys\libraries\Log.php 72
